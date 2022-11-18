c0018bf77f2ff6219f9b80f1380b3b2cb31262b3
Mako <starykovskyeric@gmail.com>
2022-11-18 13:27:41 -0600

    updated js with functions for buttons and input, added background image and form

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,35 +1,27 @@
-/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    Object Oriented JavaScript
-    Mako Starykovs'ky
-
-    The audio object
-
-    The Audio() constructor creates and returns a new HTMLAudioElement which can
-    be either attached to a document for the user to interact with and/or listen
-    to, or can be used offscreen to manage and play audio
-
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
 'use strict';
 
-const start = new Audio('./assets/audio/underground.mp3');
-start.type = 'audio/mp3';
+// Utility functions
+function onEvent(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
 
-const jump = new Audio('./assets/audio/jump.mp3');
+function select(selector, parent = document) {
+    return parent.querySelector(selector);
+}
+
+function sleep(duration) {
+    return new Promise(resolve => {
+        setTimeout(resolve, duration);
+    })
+}
+
+const alarm = new Audio('./assets/audio/alarm.[ADD FORMAT]');
 start.type = 'audio/wav';
 
-const btnOne = document.querySelector('.start');
-const btnTwo = document.querySelector('.jump');
-const mario = document.querySelector('.mario');
+const clock = select('.clock');
+const btn = select('.btn');
+const alarmSet = select('.alarm-set');
 
-btnOne.addEventListener('click', function() {
-    start.play();
-    mario.style.visibility = 'visible';
-});
-
-btnTwo.addEventListener('click', function() {
-    mario.classList.add('anim');
-    setTimeout(() => { mario.classList.remove('anim')}, 800);
-    jump.play();
+onEvent('click', btn, function() {
+    
 });
